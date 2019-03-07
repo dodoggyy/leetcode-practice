a5833d33f97ee33c05ba263f14104f5423435ff8
Chris.Lin <b9832013@hotmail.com>
2019-03-07 23:16:20 +0800

    [Java][easy][88] Merge Sorted Array
    Add only 1 while loop version
    Runtime: 2 ms, faster than 100.00% of Java
    Memory Usage: 37.5 MB, less than 6.85% of Java

--- src/com/easy/MergeSortedArray_88.java
+++ src/com/easy/MergeSortedArray_88.java
@@ -5,7 +5,7 @@ package com.easy;
 
 /**
  * @author Chris Lin
- * @version 1.0
+ * @version 1.1
  */
 public class MergeSortedArray_88 {
 
@@ -19,18 +19,37 @@ public class MergeSortedArray_88 {
         int[] tmp = new int[m + n];
 
         while (mIndexM < m && mIndexN < n) { // ref list merge method
-            tmp[mIndexTmp++] = (nums1[mIndexM] > nums2[mIndexN])?nums2[mIndexN++]:nums1[mIndexM++];
+            tmp[mIndexTmp++] = (nums1[mIndexM] > nums2[mIndexN]) ? nums2[mIndexN++] : nums1[mIndexM++];
         }
-        while(mIndexM < m) { // add remain data from nums1 to tmp
+        while (mIndexM < m) { // add remain data from nums1 to tmp
             tmp[mIndexTmp++] = nums1[mIndexM++];
         }
-        while(mIndexN < n) { // add remain data from nums2 to tmp
+        while (mIndexN < n) { // add remain data from nums2 to tmp
             tmp[mIndexTmp++] = nums2[mIndexN++];
         }
 
-        for (int i = 0; i < m+n; i++) { // move entire merged list to nums1
+        for (int i = 0; i < m + n; i++) { // move entire merged list to nums1
             nums1[i] = tmp[i];
         }
     }
 
+    // Runtime: 2 ms, faster than 100.00% of Java
+    // Memory Usage: 37.5 MB, less than 6.85% of Java
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int mIndexM = (m - 1);
+        int mIndexN = (n - 1);
+        int mIndexTmp = (m + n - 1);
+
+        while (mIndexM >= 0 || mIndexN >= 0) {
+            if (mIndexM < 0) {
+                nums1[mIndexTmp--] = nums2[mIndexN--];
+            } else if (mIndexN < 0) {
+                nums1[mIndexTmp--] = nums1[mIndexM--];
+            } else if (nums1[mIndexM] >= nums2[mIndexN]) {
+                nums1[mIndexTmp--] = nums1[mIndexM--];
+            } else {
+                nums1[mIndexTmp--] = nums2[mIndexN--];
+            }
+        }
+    }
 }
