2c9ed47ecd46a018545234245dd434f94581a0f1
Chris.Lin <b9832013@hotmail.com>
2019-07-03 20:04:20 +0800

    [Java][medium][144] Binary Tree Preorder Traversal
    Iterative version (DLR)
    Time Complexity: O(n)
    Space Complexity: O(n)
    Runtime: 0 ms, faster than 100.00%
    Memory Usage: 34.7 MB, less than 99.98%

--- src/com/medium/BinaryTreePreorderTraversal_144.java
+++ src/com/medium/BinaryTreePreorderTraversal_144.java
@@ -5,13 +5,14 @@ package com.medium;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @author Chris Lin
  * @version 1.0
  */
 public class BinaryTreePreorderTraversal_144 {
-    // Use preorder traversal (DLR)
+    // Recursive version (DLR)
     // Time Complexity: O(n)
     // Space Complexity: O(n)
     // Runtime: 0 ms, faster than 100.00%
@@ -25,6 +26,27 @@ public class BinaryTreePreorderTraversal_144 {
         mAnswer.add(root.val);
         preorderTraversal(root.left);
         preorderTraversal(root.right);
+
+        return mAnswer;
+    }
+
+    // Iterative version (DLR)
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+    // Runtime: 0 ms, faster than 100.00%
+    // Memory Usage: 34.7 MB, less than 99.98%
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        Stack<TreeNode> mStack = new Stack<>();
+        mStack.push(root);
+        while (!mStack.isEmpty()) {
+            TreeNode mNode = mStack.pop();
+            if (mNode == null) {
+                continue;
+            }
+            mAnswer.add(mNode.val);
+            mStack.push(mNode.right);
+            mStack.push(mNode.left);
+        }
 
         return mAnswer;
     }
