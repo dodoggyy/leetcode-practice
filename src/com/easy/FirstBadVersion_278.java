66ad4919207db65be70d9a61eb048534646096cb
Chris.Lin <b9832013@hotmail.com>
2019-07-15 18:31:42 +0800

    [Java][easy][278] First Bad Version
    Use binary search with recursion
    Time Complexity: O(logn)
    Space Complexity:O(logn)
    Runtime: 11 ms, faster than 30.16%
    Memory Usage: 33 MB, less than 100.00%

--- src/com/easy/FirstBadVersion_278.java
+++ src/com/easy/FirstBadVersion_278.java
@@ -13,7 +13,7 @@ public class FirstBadVersion_278 {
     }
 
     // 2019年7月15日
-    // Use binary search
+    // Use binary search with iterative
     // Time Complexity: O(logn)
     // Space Complexity:O(1)
     // Runtime: 10 ms, faster than 99.62%
@@ -32,5 +32,26 @@ public class FirstBadVersion_278 {
         }
 
         return mLow;
+    }
+
+    // 2019年7月15日
+    // Use binary search with recursion
+    // Time Complexity: O(logn)
+    // Space Complexity:O(logn)
+    // Runtime: 11 ms, faster than 30.16%
+    // Memory Usage: 33 MB, less than 100.00%
+    public int firstBadVersion2(int n) {
+        return firstBadVersion(n, 1 , n);
+    }
+    public int firstBadVersion(int n, int mLow, int mHigh) {
+        if(mLow >= mHigh) {
+            return isBadVersion(mLow)? mLow: Integer.MAX_VALUE;
+        }
+        int mMid = mLow + (mHigh - mLow)/2 ;
+        if(isBadVersion(mMid)) {
+            return firstBadVersion(n, mLow, mMid);
+        } else {
+            return firstBadVersion(n, mMid + 1, mHigh);
+        }
     }
 }
