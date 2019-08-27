ee2f32bf7277c379860a7a21292f21b6e1df6c17
Chris.Lin <b9832013@hotmail.com>
2019-08-27 22:29:33 +0800

    [Java][leetcode][easy][141] Linked List Cycle
    Use two pointer 2:
    Time Complexity: O(n)
    Space Complexity:O(n)
    Runtime: 0 ms, faster than 100.00%
    Memory Usage: 36.9 MB, less than 100.00%
    	
    Use HashSet:
    Time Complexity: O(n)
    Space Complexity:O(n)
    Runtime: 4 ms, faster than 20.20%
    Memory Usage: 37.1 MB, less than 100.00%

--- src/com/easy/LinkedListCycle_141.java
+++ src/com/easy/LinkedListCycle_141.java
@@ -3,12 +3,17 @@
  */
 package com.easy;
 
+import java.util.HashSet;
+
 /**
  * @author Chris Lin
- * @version 1.0
+ * @version 1.1
  */
 public class LinkedListCycle_141 {
 
+    // Use two pointer
+    // Time Complexity: O(n)
+    // Space Complexity:O(n)
     // Runtime: 0 ms, faster than 100.00%
     // Memory Usage: 38.2 MB, less than 93.78%
     public boolean hasCycle(ListNode head) {
@@ -26,6 +31,50 @@ public class LinkedListCycle_141 {
             }
             mNode1 = mNode1.next;
             mNode2 = mNode2.next.next;
+        }
+
+        return false;
+    }
+
+    // 2019年8月27日
+    // Use two pointer 2
+    // Time Complexity: O(n)
+    // Space Complexity:O(n)
+    // Runtime: 0 ms, faster than 100.00%
+    // Memory Usage: 36.9 MB, less than 100.00%
+    public boolean hasCycle2(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode mFast = head.next;
+        ListNode mSlow = head;
+
+        while (mFast != mSlow) {
+            if (mFast == null || mFast.next == null) {
+                return false;
+            }
+            mFast = mFast.next.next;
+            mSlow = mSlow.next;
+        }
+        return true;
+    }
+
+    // 2019年8月27日
+    // Use HashSet
+    // Time Complexity: O(n)
+    // Space Complexity:O(n)
+    // Runtime: 4 ms, faster than 20.20%
+    // Memory Usage: 37.1 MB, less than 100.00%
+    public boolean hasCycle3(ListNode head) {
+        HashSet<ListNode> mSet = new HashSet<>();
+
+        while (head != null) {
+            if (mSet.contains(head)) {
+                return true;
+            } else {
+                mSet.add(head);
+                head = head.next;
+            }
         }
 
         return false;
