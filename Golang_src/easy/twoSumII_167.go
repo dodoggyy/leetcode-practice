18bf246fa86d50a9b96e9bd1a6bfc9fcd57f2fee
Chris.Lin <b9832013@hotmail.com>
2020-10-05 02:11:42 +0800

    [Go][leetcode][easy][167] Two Sum II
    Use two pointers:
    Time Complexity: O(n)
    Space Complexity:O(1)
    Runtime: 4 ms, faster than 93.43%
    Memory Usage: 3 MB, less than 98.36%

---
+++ Golang_src/easy/twoSumII_167.go
@@ -0,0 +1,23 @@
+package easy
+
+// Use two pointers
+// Time Complexity: O(n)
+// Space Complexity:O(1)
+// Runtime: 4 ms, faster than 93.43%
+// Memory Usage: 3 MB, less than 98.36%
+func twoSum(numbers []int, target int) []int {
+	size := len(numbers)
+	left, right := 0, size-1
+
+	for left < right {
+		current := numbers[left] + numbers[right]
+		if current < target {
+			left++
+		} else if current > target {
+			right--
+		} else {
+			return []int{left + 1, right + 1}
+		}
+	}
+	return []int{}
+}
