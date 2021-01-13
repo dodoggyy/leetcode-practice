b6b2e2ec0b3b1a53f32a11f33d2563d912f66240
Chris.Lin <b9832013@hotmail.com>
2021-01-13 22:40:17 +0800

    [Go][leetcode][medium][19] Remove Nth Node From End of List
    Use two pointers:
    Time Complexity: O(L)
    Space Complexity:O(1)
    Runtime: 0 ms, faster than 100.00%
    Memory Usage: 2.2 MB, less than 21.37%
    
    Use two pointers enhance version:
    Time Complexity: O(L)
    Space Complexity:O(1)
    Runtime: 0 ms, faster than 100.00%
    Memory Usage: 2.2 MB, less than 21.37%

---
+++ Golang_src/medium/removeNthNodeFromEndOfList_19.go
@@ -0,0 +1,54 @@
+package medium
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// Use two pointers:
+// Time Complexity: O(L)
+// Space Complexity:O(1)
+// Runtime: 0 ms, faster than 100.00%
+// Memory Usage: 2.2 MB, less than 21.37%
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	if head.Next == nil {
+		return nil
+	}
+
+	slow, fast := head, head
+	for n != 0 {
+		fast = fast.Next
+		n--
+	}
+	if fast == nil {
+		return head.Next
+	}
+
+	for fast.Next != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+
+	slow.Next = slow.Next.Next
+
+	return head
+}
+
+// Use two pointers enhance version:
+// Time Complexity: O(L)
+// Space Complexity:O(1)
+// Runtime: 0 ms, faster than 100.00%
+// Memory Usage: 2.2 MB, less than 21.37%
+func removeNthFromEnd2(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{Val: 0, Next: head}
+	slow, fast := dummy, head
+	for i := 0; i < n; i++ {
+		fast = fast.Next
+	}
+	for ; fast != nil; fast = fast.Next {
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+
+	return dummy.Next
+}
