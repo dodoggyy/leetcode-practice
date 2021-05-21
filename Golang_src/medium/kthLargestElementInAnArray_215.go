ccbdf1dd7e7c0e096aa1073384689ec59a28826e
Chris.Lin <b9832013@hotmail.com>
2021-05-22 02:35:20 +0800

    [Go][leetcode][medium][215] Kth Largest Element in an Array
    Use quick select 2:
    Time Complexity: O(n)
    Space Complexity:O(1)
    Runtime: 32 ms, faster than 5.33%
    Memory Usage: 3.9 MB, less than 42.13%

--- Golang_src/medium/kthLargestElementInAnArray_215.go
+++ Golang_src/medium/kthLargestElementInAnArray_215.go
@@ -27,6 +27,39 @@ func quickSelect(nums *[]int, indexLow, indexHigh, k int) int {
 	} else if count > k {
 		return quickSelect(nums, i+1, indexHigh, k)
 	} else {
-		return quickSelect(nums, indexLow, i - 1, k-count)
+		return quickSelect(nums, indexLow, i-1, k-count)
 	}
+}
+
+// Use quick select 2:
+// Time Complexity: O(n)
+// Space Complexity:O(1)
+// Runtime: 32 ms, faster than 5.33%
+// Memory Usage: 3.9 MB, less than 42.13%
+func findKthLargest2(nums []int, k int) int {
+	var qSelect func(start, end, k int) int
+	qSelect = func(start, end, k int) int {
+		i := start - 1
+
+		// O(n)
+		for j := start; j < end; j++ {
+			if nums[j] < nums[end] {
+				i++
+				nums[i], nums[j] = nums[j], nums[i]
+			}
+		}
+		i++
+		nums[end], nums[i] = nums[i], nums[end]
+
+		cnt := end - i + 1
+		if cnt == k {
+			return nums[i]
+		} else if cnt < k {
+			return qSelect(start, i-1, k-cnt)
+		} else {
+			return qSelect(i+1, end, k)
+		}
+	}
+
+	return qSelect(0, len(nums)-1, k)
 }
