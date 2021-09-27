e0246f0ca29e773a1430b7a68e8206dbebbb3153
Chris Lin <b9832013@hotmail.com>
2021-09-28 00:32:07 +0800

    [Go][leetcode][medium][91] Decode Ways
    Use optimize DP 2:
    Time Complexity: O(n)
    Space Complexity:O(1)
    Runtime: 0 ms, faster than 100.00%
    Memory Usage: 2.1 MB, less than 82.90%
    
    Use DP:
    Time Complexity: O(n)
    Space Complexity:O(n)
    Runtime: 0 ms, faster than 100.00%
    Memory Usage: 2 MB, less than 82.90%

--- Golang_src/medium/decodeWays_91.go
+++ Golang_src/medium/decodeWays_91.go
@@ -1,6 +1,6 @@
 package medium
 
-// Use DP:
+// Use optimize DP:
 // Time Complexity: O(n)
 // Space Complexity:O(1)
 // Runtime: 0 ms, faster than 100.00%
@@ -27,5 +27,52 @@ func numDecodings(s string) int {
 			pre = cur
 		}
 	}
+	return cur
+}
+
+// Use optimize DP 2:
+// Time Complexity: O(n)
+// Space Complexity:O(1)
+// Runtime: 0 ms, faster than 100.00%
+// Memory Usage: 2.1 MB, less than 82.90%
+func numDecodings2(s string) int {
+	size := len(s)
+	dp := make([]int, size+1)
+
+	dp[0] = 1
+
+	for i := 1; i <= size; i++ {
+		if s[i-1] != '0' {
+			dp[i] += dp[i-1]
+		}
+		if i > 1 && s[i-2] != '0' && 10*(s[i-2]-'0')+(s[i-1]-'0') <= 26 {
+			dp[i] += dp[i-2]
+		}
+	}
+
+	return dp[size]
+}
+
+// Use DP:
+// Time Complexity: O(n)
+// Space Complexity:O(n)
+// Runtime: 0 ms, faster than 100.00%
+// Memory Usage: 2 MB, less than 82.90%
+func numDecodings3(s string) int {
+	size := len(s)
+	pre1, pre2, cur := 0, 1, 0
+
+	for i := 1; i <= size; i++ {
+		cur = 0
+		if s[i-1] != '0' {
+			cur += pre2
+		}
+		if i > 1 && s[i-2] != '0' && 10*(s[i-2]-'0')+(s[i-1]-'0') <= 26 {
+			cur += pre1
+		}
+		pre1 = pre2
+		pre2 = cur
+	}
+
 	return cur
 }
