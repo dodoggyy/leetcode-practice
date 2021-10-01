fdc9f702613a348069e947427f30c7faacc18975
Chris Lin <b9832013@hotmail.com>
2021-10-01 22:47:56 +0800

    [Go][leetcode][easy][3] Longest Substring Without Repeating Characters
    Use slide window2:
    Time Complexity: O(n)
    Space Complexity:O(k)
    k for character set
    Runtime: 3 ms, faster than 90.23%
    Memory Usage: 2.7 MB, less than 100.00%

--- Golang_src/medium/longestSubstringWithoutRepeatingCharacters_3.go
+++ Golang_src/medium/longestSubstringWithoutRepeatingCharacters_3.go
@@ -23,9 +23,26 @@ func lengthOfLongestSubstring(s string) int {
 	return result
 }
 
-func max(a, b int) int {
-	if a > b {
-		return a
+// Use slide window2:
+// Time Complexity: O(n)
+// Space Complexity:O(k)
+// k for character set
+// Runtime: 3 ms, faster than 90.23%
+// Memory Usage: 2.7 MB, less than 100.00%
+func lengthOfLongestSubstring2(s string) int {
+	arr := make([]int, 128)
+	result := 0
+
+	l, r := 0, 0
+
+	for r < len(s) {
+		idx := s[r]
+		l = max(l, arr[idx])
+		result = max(result, r-l+1)
+
+		arr[idx] = r + 1
+		r++
 	}
-	return b
+
+	return result
 }
