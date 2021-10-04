77b4f2378fc78a2b611827dfa66bbd72cd5f6591
Chris Lin <b9832013@hotmail.com>
2021-10-04 22:11:22 +0800

    [Go][leetcode][medium][54] Spiral Matrix
    Use Layer-by-Layer 2:
    Time Complexity: O(m*n)
    Space Complexity:O(1)
    Runtime: 0 ms, faster than 100.00%
    Memory Usage: 2.1 MB, less than 52.82%

--- Golang_src/medium/spiralMatrix_54.go
+++ Golang_src/medium/spiralMatrix_54.go
@@ -35,3 +35,43 @@ func spiralOrder(matrix [][]int) []int {
 
 	return result
 }
+
+// Use Layer-by-Layer 2:
+// Time Complexity: O(m*n)
+// Space Complexity:O(1)
+// Runtime: 0 ms, faster than 100.00%
+// Memory Usage: 2.1 MB, less than 52.82%
+func spiralOrder2(matrix [][]int) []int {
+	m, n := len(matrix), len(matrix[0])
+	left, right := 0, n-1
+	top, bottom := 0, m-1
+
+	result := []int{}
+
+	for left <= right && top <= bottom {
+		for i := left; i < right; i++ {
+			result = append(result, matrix[top][i])
+		}
+		for i := top; i <= bottom; i++ {
+			result = append(result, matrix[i][right])
+		}
+
+		// consider last center may not have cases as below
+		if left < right && top < bottom {
+			for i := right - 1; i > left; i-- {
+				result = append(result, matrix[bottom][i])
+			}
+
+			for i := bottom; i > top; i-- {
+				result = append(result, matrix[i][left])
+			}
+		}
+
+		left++
+		right--
+		top++
+		bottom--
+	}
+
+	return result
+}
