abb31f2b1cae5248d477a8cae1cdc6eb11f1ef9d
Chris Lin <b9832013@hotmail.com>
2021-10-08 01:34:35 +0800

    [Go][leetcode][medium][105] Construct Binary Tree from Preorder and Inorder Traversal
    Use recursive:
    Time Complexity: O(n)
    Space Complexity:O(n)
    Runtime: 4 ms, faster than 89.52%
    Memory Usage: 4.3 MB, less than 69.52%

--- Golang_src/medium/constructBinaryTreeFromPreorderAndInorderTraversal_105.go
+++ Golang_src/medium/constructBinaryTreeFromPreorderAndInorderTraversal_105.go
@@ -3,22 +3,31 @@ package medium
 // Use recursive:
 // Time Complexity: O(n)
 // Space Complexity:O(n)
-// Runtime: 36 ms, faster than 23.67%
-// Memory Usage: 24.3 MB, less than 13.78%
-func buildTree(preorder []int, inorder []int) *TreeNode {
+// Runtime: 4 ms, faster than 89.52%
+// Memory Usage: 4.3 MB, less than 69.52%
+func buildTree2(preorder []int, inorder []int) *TreeNode {
 	if len(preorder) == 0 {
 		return nil
 	}
+
 	root := &TreeNode{Val: preorder[0]}
 
-	index := 0
-	for ; index < len(inorder); index++ {
-		if inorder[index] == preorder[0] {
-			break
-		}
-	}
-	root.Left = buildTree(preorder[1:len(inorder[:index])+1], inorder[:index])
-	root.Right = buildTree(preorder[len(inorder[:index])+1:], inorder[index+1:])
+	idx := findRoot(inorder, preorder[0])
+	root.Left = buildTree(preorder[1:len(inorder[:idx])+1], inorder[:idx])
+	root.Right = buildTree(preorder[len(inorder[:idx])+1:], inorder[idx+1:])
 
 	return root
+}
+
+// pre: D -> L -> R
+//  in: L -> D -> R
+
+func findRoot(inorder []int, target int) int {
+	for i := range inorder {
+		if inorder[i] == target {
+			return i
+		}
+	}
+
+	return -1
 }
