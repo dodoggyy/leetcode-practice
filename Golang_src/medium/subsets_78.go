c23ffc8a54921253771e00bcefbf67799e85ef7a
Chris Lin <b9832013@hotmail.com>
2021-10-23 21:58:56 +0800

    [Go][leetcode][medium][78] Subsets
    Use backtracking:
    Time Complexity: O(n*2^n)
    Space Complexity:O(n)
    Runtime: 2 ms, faster than 27.30%
    Memory Usage: 2.6 MB, less than 20.73%

--- Golang_src/medium/subsets_78.go
+++ Golang_src/medium/subsets_78.go
@@ -5,33 +5,31 @@ import "math"
 // Use backtracking:
 // Time Complexity: O(n*2^n)
 // Space Complexity:O(n)
-// Runtime: 0 ms, faster than 100.00%
-// Memory Usage: 2.4 MB, less than 16.25%
+// Runtime: 2 ms, faster than 27.30%
+// Memory Usage: 2.6 MB, less than 20.73%
 func subsets(nums []int) [][]int {
-	size := len(nums)
 	result := [][]int{}
-	if size == 0 {
-		return result
-	}
+	keep := []int{}
+	size := len(nums)
 
-	var dfs func(int, int, []int)
-	dfs = func(idxCur, subsetSize int, subset []int) {
-		if subsetSize == 0 {
-			tmp := make([]int, len(subset))
-			copy(tmp, subset)
+	var dfs func(idx, subSize int)
+	dfs = func(idx, subSize int) {
+		if subSize == 0 {
+			tmp := make([]int, len(keep))
+			copy(tmp, keep)
 			result = append(result, tmp)
 			return
 		}
 
-		for i := idxCur; i < size; i++ {
-			subset = append(subset, nums[i])
-			dfs(i+1, subsetSize-1, subset)
-			subset = subset[:len(subset)-1]
+		for i := idx; i < size; i++ {
+			keep = append(keep, nums[i])
+			dfs(i+1, subSize-1)
+			keep = keep[:len(keep)-1]
 		}
 	}
 
 	for i := 0; i <= size; i++ {
-		dfs(0, i, []int{})
+		dfs(0, i)
 	}
 
 	return result
