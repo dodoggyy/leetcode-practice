7d80fd068e13264084df673af6e8ab99e6bfcc87
dodoggyy <b9832013@hotmail.com>
2022-06-26 03:56:23 +0800

    [Go][leetcode][medium][665] Non-decreasing Array
    Use Reduce to Smaller Problem:
    Time Complexity: O(n)
    Space Complexity:O(1)
    Runtime: 35 ms, faster than 48.84%
    Memory Usage: 6.6 MB, less than 74.42%

--- Golang_src/easy/nonDecreasingArray_665.go
+++ Golang_src/easy/nonDecreasingArray_665.go
@@ -3,14 +3,17 @@ package easy
 // Use Reduce to Smaller Problem:
 // Time Complexity: O(n)
 // Space Complexity:O(1)
-// Runtime: 20 ms, faster than 98.96%
-// Memory Usage: 6.3 MB, less than 34.38%
+// Runtime: 35 ms, faster than 48.84%
+// Memory Usage: 6.6 MB, less than 74.42%
 func checkPossibility(nums []int) bool {
 	count := 0
 	for i := 1; i < len(nums); i++ {
 		if nums[i] < nums[i-1] {
 			count++
-			if i-2 >= 0 && nums[i-2] > nums[i] {
+			if count > 1 {
+				return false
+			}
+			if i > 1 && nums[i-2] > nums[i] {
 				nums[i] = nums[i-1]
 			} else {
 				nums[i-1] = nums[i]
@@ -18,5 +21,5 @@ func checkPossibility(nums []int) bool {
 		}
 	}
 
-	return count <= 1
+	return true
 }
