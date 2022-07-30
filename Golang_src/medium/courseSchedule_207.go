155703183e15d55d961f6f51b69f6d0067949ef9
Chris Lin <b9832013@hotmail.com>
2022-07-31 00:02:50 +0800

    [Go][leetcode][medium][207] Course Schedule
    Use Topological Sort via DFS:
    Time Complexity: O(n+m)
    Space Complexity:O(n+m)
    Runtime: 26 ms, faster than 23.60%
    Memory Usage: 6.4 MB, less than 77.00%

--- Golang_src/medium/courseSchedule_207.go
+++ Golang_src/medium/courseSchedule_207.go
@@ -1,8 +1,8 @@
 package medium
 
-// Use Topological Sort:
-// Time Complexity: O(n)
-// Space Complexity:O(n)
+// Use Topological Sort via DFS:
+// Time Complexity: O(n+m)
+// Space Complexity:O(n+m)
 // Runtime: 8 ms, faster than 96.25%
 // Memory Usage: 6.1 MB, less than 41.57%
 func canFinish(numCourses int, prerequisites [][]int) bool {
@@ -42,4 +42,49 @@ func hasCycle2(cur int, visit *[]int, graph *[][]int) bool {
 	(*visit)[cur] = 2 // 2 = visited
 
 	return false
+}
+
+// Use Topological Sort via DFS:
+// Time Complexity: O(n+m)
+// Space Complexity:O(n+m)
+// Runtime: 26 ms, faster than 23.60%
+// Memory Usage: 6.4 MB, less than 77.00%
+func canFinish2(numCourses int, prerequisites [][]int) bool {
+	graph := make([][]int, numCourses)
+	for i := range prerequisites {
+		graph[prerequisites[i][1]] = append(graph[prerequisites[i][1]], prerequisites[i][0])
+	}
+
+	// state: 0 = not visit, 1 = visiting, 2 = visited
+	visit := make([]int, numCourses)
+
+	var hasCycle func(cur int) bool
+	hasCycle = func(cur int) bool {
+		if visit[cur] == 1 {
+			return true
+		}
+		if visit[cur] == 2 {
+			return false
+		}
+
+		visit[cur] = 1
+
+		for i := 0; i < len(graph[cur]); i++ {
+			if hasCycle(graph[cur][i]) {
+				return true
+			}
+		}
+
+		visit[cur] = 2
+
+		return false
+	}
+
+	for i := 0; i < numCourses; i++ {
+		if hasCycle(i) {
+			return false
+		}
+	}
+
+	return true
 }
