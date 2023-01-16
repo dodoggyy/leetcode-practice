814b15d970ed286a99d52350c85ec6c2da5c8ad9
Chris Lin <b9832013@hotmail.com>
2023-01-16 21:17:58 +0800

    [Go][leetcode][easy][392] Is Subsequence
    Use two pointers:
    Time Complexity: O(n)
    Space Complexity:O(1)
    Runtime: 1 ms, faster than 66.70%
    Memory Usage: 1.9 MB, less than 100%

--- Golang_src/easy/isSubsequence_392.go
+++ Golang_src/easy/isSubsequence_392.go
@@ -25,3 +25,24 @@ func isSubsequence(s string, t string) bool {
 
 	return false
 }
+
+// Use two pointers:
+// Time Complexity: O(n)
+// Space Complexity:O(1)
+// Runtime: 1 ms, faster than 66.70%
+// Memory Usage: 1.9 MB, less than 100%
+func isSubsequence2(s string, t string) bool {
+	idx1, idx2 := 0, 0
+	size1, size2 := len(s), len(t)
+
+	for idx1 < size1 && idx2 < size2 {
+		if s[idx1] == t[idx2] {
+			idx1++
+			idx2++
+		} else {
+			idx2++
+		}
+	}
+
+	return idx1 == size1
+}
