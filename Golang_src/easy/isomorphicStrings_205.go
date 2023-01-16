02cbf3624da6a30b8cc1e935e3476e3b071538b8
Chris Lin <b9832013@hotmail.com>
2023-01-16 21:20:30 +0800

    [Go][leetcode][easy][205]Isomorphic Strings
    Use Array count:
    Time Complexity: O(n)
    Space Complexity:O(1)
    Runtime: 4 ms, faster than 58.49%
    Memory Usage: 3.4 MB, less than 14.15%

--- Golang_src/easy/isomorphicStrings_205.go
+++ Golang_src/easy/isomorphicStrings_205.go
@@ -6,19 +6,17 @@ package easy
 // Runtime: 4 ms, faster than 58.49%
 // Memory Usage: 3.4 MB, less than 14.15%
 func isIsomorphic(s string, t string) bool {
-	strS := []rune(s)
-	strT := []rune(t)
-
+	if len(s) != len(t) {
+		return false
+	}
 	countS, countT := [256]int{}, [256]int{}
 
-	length := len(strS)
-
-	for i := 0; i < length; i++ {
-		if countS[strS[i]] != countT[strT[i]] {
+	for i := range s {
+		if countS[s[i]] != countT[t[i]] {
 			return false
 		}
-		countS[strS[i]] = (i + 1)
-		countT[strT[i]] = (i + 1)
+		countS[s[i]] = (i + 1)
+		countT[t[i]] = (i + 1)
 
 	}
 
