0cb9cabd761808f2f81398a42317e9e2d7ababc4
Chris Lin <b9832013@hotmail.com>
2023-04-04 02:31:01 +0800

    [Go][leetcode][medium][524]Longest Word in Dictionary through Deleting
    Use two pointers 2:
    Time Complexity: O(d*(m+n))
    Space Complexity:O(1)
    Runtime: 12 ms, faster than 93.75%
    Memory Usage: 8.1 MB, less than 25%

--- Golang_src/medium/LongestWordInDictionaryThroughDeleting_524.go
+++ Golang_src/medium/LongestWordInDictionaryThroughDeleting_524.go
@@ -10,7 +10,7 @@ import "strings"
 // Runtime: 16 ms, faster than 68.97%
 // Memory Usage: 6.4 MB, less than 89.66%
 func findLongestWord(s string, d []string) string {
-	var result string = ""
+	result := ""
 	target := []rune(s)
 
 	for _, str := range d {
@@ -37,4 +37,29 @@ func checkSubStr(target, str []rune) bool {
 	}
 
 	return j == len(str)
+}
+
+// Use two pointers 2:
+// Time Complexity: O(d*(m+n))
+// Space Complexity:O(1)
+// Runtime: 12 ms, faster than 93.75%
+// Memory Usage: 8.1 MB, less than 25%
+func findLongestWord2(s string, dictionary []string) string {
+	result := ""
+	for _, t := range dictionary {
+		i := 0
+		for j := range s {
+			if s[j] == t[i] {
+				i++
+				if i == len(t) {
+					if len(t) > len(result) || len(t) == len(result) && t < result {
+						result = t
+					}
+					break
+				}
+			}
+		}
+	}
+
+	return result
 }
