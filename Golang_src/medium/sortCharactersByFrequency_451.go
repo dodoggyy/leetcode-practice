ab6f12b3fc836202f4a75bc2f83609c5e22a06ba
Chris Lin <b9832013@hotmail.com>
2023-04-05 23:33:52 +0800

    [Go][leetcode][medium][451] Sort Characters By Frequency
    Use Multiset and Bucket Sort:
    Time Complexity: O(n)
    Space Complexity:O(n)
    Runtime: 3 ms, faster than 94.78%
    Memory Usage: 5.8 MB, less than 41.74%

--- Golang_src/medium/sortCharactersByFrequency_451.go
+++ Golang_src/medium/sortCharactersByFrequency_451.go
@@ -3,8 +3,8 @@ package medium
 import "sort"
 
 // Use hashmap and sort:
-// Time Complexity: O(n)
-// Space Complexity:O(1)
+// Time Complexity: O(nlogn)
+// Space Complexity:O(n)
 // Runtime: 8 ms, faster than 57.81%
 // Memory Usage: 6 MB, less than 26.56%
 func frequencySort(s string) string {
@@ -31,6 +31,45 @@ func frequencySort(s string) string {
 		for j := 0; j < freq; j++ {
 			result = append(result, v)
 		}
+	}
+
+	return string(result)
+}
+
+// Use Multiset and Bucket Sort:
+// Time Complexity: O(n)
+// Space Complexity:O(n)
+// Runtime: 3 ms, faster than 94.78%
+// Memory Usage: 5.8 MB, less than 41.74%
+func frequencySor2(s string) string {
+	hashmap := map[byte]int{}
+
+	for i := range s {
+		hashmap[s[i]]++
+	}
+
+	hashmap2 := map[int][]byte{}
+
+	for k, v := range hashmap {
+		if val, ok := hashmap2[v]; ok {
+			val = append(val, k)
+			hashmap2[v] = val
+		} else {
+			hashmap2[v] = []byte{k}
+		}
+	}
+
+	result := []byte{}
+	cur := len(s)
+	for len(result) < len(s) {
+		if val, ok := hashmap2[cur]; ok {
+			for _, ch := range val {
+				for j := 0; j < cur; j++ {
+					result = append(result, ch)
+				}
+			}
+		}
+		cur--
 	}
 
 	return string(result)
