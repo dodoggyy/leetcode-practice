9cbe9f0e94e7dfb6941ca2358f634e9cd493c032
Chris Lin <b9832013@hotmail.com>
2023-05-05 23:52:43 +0800

    [Go][leetcode][medium][462] Minimum Moves to Equal Array Elements II
    Use quick select 2:
    Time Complexity: O(n)
    Space Complexity:O(1)
    Runtime: 149 ms, faster than 8.70%
    Memory Usage: 5.2 MB, less than 100%

--- Golang_src/medium/minimumMovesToEqualArrayElementsII_462.go
+++ Golang_src/medium/minimumMovesToEqualArrayElementsII_462.go
@@ -84,3 +84,49 @@ func findMedium(nums []int, index int) int {
 		}
 	}
 }
+
+// Use quick select 2:
+// Time Complexity: O(n)
+// Space Complexity:O(1)
+// Runtime: 149 ms, faster than 8.70%
+// Memory Usage: 5.2 MB, less than 100%
+func minMoves3(nums []int) int {
+	size := len(nums)
+
+	var qs func(l, r, k int) int
+	qs = func(l, r, k int) int {
+		i := l - 1
+		for j := l; j < r; j++ {
+			if nums[j] < nums[r] {
+				i++
+				nums[j], nums[i] = nums[i], nums[j]
+			}
+		}
+		i++
+		nums[i], nums[r] = nums[r], nums[i]
+
+		if i == k {
+			return nums[i]
+		} else if i < k {
+			return qs(i+1, r, k)
+		} else {
+			return qs(l, i-1, k)
+		}
+	}
+
+	abs := func(a int) int {
+		if a < 0 {
+			return -a
+		}
+		return a
+	}
+
+	mid := qs(0, size-1, size/2)
+
+	result := 0
+	for _, v := range nums {
+		result += abs(mid - v)
+	}
+
+	return result
+}
