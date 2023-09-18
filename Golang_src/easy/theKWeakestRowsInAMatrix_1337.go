5f67cc759bafb76120625424b2eb8522c7e45c00
Chris Lin <b9832013@hotmail.com>
2023-09-18 19:38:28 +0800

    [Go][leetcode][easy][1337] The K Weakest Rows in a Matrix
    Use binary search + hashmap:
    m: matrix row
    n: matrix coloumn
    Time Complexity: O(m*logn + k)
    Space Complexity:O(m)
    Runtime: 12 ms, faster than 46.75%
    Memory Usage: 5.05 MB, less than 20.78%

--- Golang_src/easy/theKWeakestRowsInAMatrix_1337.go
+++ Golang_src/easy/theKWeakestRowsInAMatrix_1337.go
@@ -28,3 +28,53 @@ func kWeakestRows(mat [][]int, k int) []int {
 
 	return idx[:k]
 }
+
+// Use binary search + hashmap:
+// m: matrix row
+// n: matrix coloumn
+// Time Complexity: O(m*logn + k)
+// Space Complexity:O(m)
+// Runtime: 12 ms, faster than 46.75%
+// Memory Usage: 5.05 MB, less than 20.78%
+func kWeakestRows2(mat [][]int, k int) []int {
+	hashmap := map[int][]int{}
+	result := []int{}
+	calRow := func(i int) {
+		cur := -1
+		l, r := 0, len(mat[i])-1
+		for l <= r {
+			mid := l + (r-l)/2
+			if mat[i][mid] == 0 {
+				r = mid - 1
+			} else {
+				cur = mid
+				l = mid + 1
+			}
+		}
+		if v, ok := hashmap[cur+1]; ok {
+			v = append(v, i)
+			hashmap[cur+1] = v
+		} else {
+			hashmap[cur+1] = []int{i}
+		}
+	}
+
+	for i := range mat {
+		calRow(i)
+	}
+
+	cnt := 0
+	for k > 0 {
+		if v, ok := hashmap[cnt]; ok {
+			if len(v) > k {
+				v = v[:k]
+			}
+			result = append(result, v...)
+			k -= len(v)
+
+		}
+		cnt++
+	}
+
+	return result
+}
