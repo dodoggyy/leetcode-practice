d30e2ddd561c0dc254e6149c8a425a0bf5b07626
Chris Lin <b9832013@hotmail.com>
2023-10-28 23:35:10 +0800

    [Go][leetcode][medium][71] Simplify Path
    Use stack 2:
    Time Complexity: O(n)
    Space Complexity:O(n)
    Runtime: 0 ms, faster than 100.00%
    Memory Usage: 3.15 MB, less than 35.84%

--- Golang_src/medium/simplifyPath_71.go
+++ Golang_src/medium/simplifyPath_71.go
@@ -1,5 +1,7 @@
 package medium
 
+import "strings"
+
 // Use stack:
 // Time Complexity: O(n)
 // Space Complexity:O(n)
@@ -46,4 +48,28 @@ func simplifyPath(path string) string {
 	}
 
 	return result[:len(result)-1]
+}
+
+// Use stack 2:
+// Time Complexity: O(n)
+// Space Complexity:O(n)
+// Runtime: 0 ms, faster than 100.00%
+// Memory Usage: 3.15 MB, less than 35.84%
+func simplifyPath2(path string) string {
+	stack := []string{}
+	pathArr := strings.Split(path, "/")
+	for _, str := range pathArr {
+		switch str {
+		case "..":
+			if len(stack) > 0 {
+				stack = stack[:len(stack)-1]
+			}
+		default:
+			if len(str) > 0 && str != "." {
+				stack = append(stack, str)
+			}
+		}
+	}
+
+	return "/" + strings.Join(stack, "/")
 }
