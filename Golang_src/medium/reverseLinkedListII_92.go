0cab90efb0483cbd2527fcaed7cf3dd56d51df45
Chris Lin <b9832013@hotmail.com>
2023-10-28 23:26:49 +0800

    [Go][leetcode][medium][92] Reverse Linked List II
    Use Iterative Link Reversal:
    Time Complexity: O(n)
    Space Complexity:O(1)
    Runtime: 1 ms, faster than 79.30%
    Memory Usage: 2.14 MB, less than 11.62%

--- Golang_src/medium/reverseLinkedListII_92.go
+++ Golang_src/medium/reverseLinkedListII_92.go
@@ -25,3 +25,47 @@ func reverseBetween(head *ListNode, left int, right int) *ListNode {
 
 	return dummy.Next
 }
+
+// Use Iterative Link Reversal:
+// Time Complexity: O(n)
+// Space Complexity:O(1)
+// Runtime: 1 ms, faster than 79.30%
+// Memory Usage: 2.14 MB, less than 11.62%
+func reverseBetween2(head *ListNode, left int, right int) *ListNode {
+	reverse := func(head *ListNode) *ListNode {
+		var pre *ListNode
+
+		for head != nil {
+			tmp := head.Next
+			head.Next = pre
+			pre = head
+			head = tmp
+		}
+		return pre
+	}
+
+	dummy := &ListNode{Next: head}
+	pre := dummy
+
+	for i := 0; i < left-1; i++ {
+		pre = pre.Next
+	}
+
+	nodeRight := pre
+	for i := left; i <= right; i++ {
+		nodeRight = nodeRight.Next
+	}
+
+	nodeLeft := pre.Next
+	cur := nodeRight.Next
+
+	pre.Next = nil
+	nodeRight.Next = nil
+
+	nodeRight = reverse(nodeLeft)
+
+	pre.Next = nodeRight
+	nodeLeft.Next = cur
+
+	return dummy.Next
+}
