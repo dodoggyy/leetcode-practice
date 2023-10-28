42fc16b0e23763bfeb7b49ec6a8b1955b24ec488
Chris Lin <b9832013@hotmail.com>
2023-10-28 22:47:13 +0800

    [Go][leetcode][medium][138] Copy List with Random Pointer
    Use hashmap 2:
    Time Complexity: O(n)
    Space Complexity:O(n)
    Runtime: 2 ms, faster than 70.25%
    Memory Usage: 3.72 MB, less than 5.83%

--- Golang_src/medium/copyListWithRandomPointer_138.go
+++ Golang_src/medium/copyListWithRandomPointer_138.go
@@ -6,7 +6,7 @@ type RNode struct {
 	Random *RNode
 }
 
-// Use DFS:
+// Use hashmap:
 // Time Complexity: O(n)
 // Space Complexity:O(n)
 // Runtime: 0 ms, faster than 100.00%
@@ -37,4 +37,30 @@ func copyRandomList(head *RNode) *RNode {
 	}
 
 	return newHead.Next
+}
+
+// Use hashmap 2:
+// Time Complexity: O(n)
+// Space Complexity:O(n)
+// Runtime: 2 ms, faster than 70.25%
+// Memory Usage: 3.72 MB, less than 5.83%
+func copyRandomList2(head *RNode) *RNode {
+	if head == nil {
+		return head
+	}
+	cur := head
+	hashmap := map[*RNode]*RNode{}
+	for cur != nil {
+		hashmap[cur] = &RNode{Val: cur.Val}
+		cur = cur.Next
+	}
+	cur = head
+	for cur != nil {
+		node := hashmap[cur]
+		node.Next = hashmap[cur.Next]
+		node.Random = hashmap[cur.Random]
+		cur = cur.Next
+	}
+
+	return hashmap[head]
 }
